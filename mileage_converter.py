27a888de3544cf29db929759ebf94c460f2dc6a4
Michael Zhu <81886513+MichaelCZhu@users.noreply.github.com>
2021-07-07 16:47:30 -0500

    Add files via upload

---
+++ mileage_converter.py
@@ -0,0 +1,11 @@
+print("Please input the number of kilometers you ran today. The program will convert it into miles.")
+kilometers = float(input())
+miles = kilometers * 0.621371
+rounded_miles = round(miles, 2)
+print("So you ran " + f"{kilometers}" + " kilometers today.")
+print("That converts to about " + f"{rounded_miles}" + " miles!")
+
+# # Random input example
+# # print("Input your age")
+# # age = input()
+# # print("You are " + age + " years old!")
